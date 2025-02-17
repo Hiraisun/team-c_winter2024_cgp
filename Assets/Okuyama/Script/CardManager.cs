51ae666717dfd92e1036126c5202854b6a8978f2
Hiraisun <koushi.2nd@gmail.com>
2025-02-17 19:46:31 +0900

    手札配置をテスト

--- Assets/Okuyama/Script/CardManager.cs
+++ Assets/Okuyama/Script/CardManager.cs
@@ -9,13 +9,35 @@ public class CardManager : MonoBehaviour
     [SerializeField] private float HandRadius;
     [SerializeField] private float HandAngle;
 
+    [SerializeField] private Vector3 CardStartPos; //ドロー時にカードが出現する位置
 
-    [SerializeField] private Vector3 CardSize;
+    [SerializeField] private GameObject CardPrefab;
+    private Vector3 CardSize
+    {
+        get { return CardPrefab.GetComponent<SpriteRenderer>().bounds.size; }
+    }
 
+
+    // ドブルカードの配列
+    // この要素数が総カード数、かつ総シンボル数
     private List<int>[] dobbleCardArr;
 
-    void Awake()
+    private List<CardObject> hand = new List<CardObject>(); // 手札
+
+    // シングルトン(簡易)
+    public static CardManager instance;
+    private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        // ドブルカードの初期化
         InitializeDobbleCardArr(4);
     }
 
@@ -24,25 +46,53 @@ public class CardManager : MonoBehaviour
 
     }
 
+    // カードを引く
     public void Draw()
     {
-        Debug.Log("Draw");
+        GameObject cardObj = Instantiate(CardPrefab, transform);
+        cardObj.transform.parent = transform; //子にする
+        cardObj.transform.position = CardStartPos; //初期位置に移動
+
+        CardObject card = cardObj.GetComponent<CardObject>();
+        card.Initialize(0); //TODO:ランダムに引く
+
+        hand.Add(card);
+        AdjustHandPos();
     }
 
-    public struct CardPos
+    private void AdjustHandPos()
     {
-        public Vector3 CenterPos;
-        public float Angle;
+        Debug.Log(hand.Count);
+        List<CardObject.CardPos> cardPosList = CalculateHandPos(hand.Count);
+        for (int i = 0; i < hand.Count; i++)
+        {
+            hand[i].SetHandPos(cardPosList[i]);
+        }
     }
+
+
     // 手札枚数に応じた手札の位置を計算
-    List<CardPos> CalculateHandPos(int handNum)
+    // TODO:手札枚数が少ないときの見栄えが悪い。要調整
+    List<CardObject.CardPos> CalculateHandPos(int handNum)
     {
-        List<CardPos> cardPosList = new List<CardPos>();
+        List<CardObject.CardPos> cardPosList = new List<CardObject.CardPos>();
+
+        // 例外処理
+        if (handNum == 0) return cardPosList;
+        if (handNum == 1)
+        {
+            CardObject.CardPos cardPos = new CardObject.CardPos();
+            cardPos.center = HandCenter + new Vector3(0, HandRadius, 0);
+            cardPos.angle = 0;
+            cardPosList.Add(cardPos);
+            return cardPosList;
+        }
+
+        //ほんへ
         for (int i = 0; i < handNum; i++)
         {
-            CardPos cardPos = new CardPos();
+            CardObject.CardPos cardPos = new CardObject.CardPos();
 
-            // 各手札の角度計算
             float angle = Mathf.Lerp(-HandAngle, HandAngle, (float)i / (handNum - 1));
 
             // 中心座標計算
@@ -50,8 +100,8 @@ public class CardManager : MonoBehaviour
             float CenterY = HandCenter.y + HandRadius * Mathf.Cos(angle * Mathf.Deg2Rad);
             float CenterZ = i * -0.1f; // 右の手札ほど手前に (UIデザイン考慮して検討)
 
-            cardPos.CenterPos = new Vector3(CenterX, CenterY, CenterZ);
-            cardPos.Angle = angle;
+            cardPos.center = new Vector3(CenterX, CenterY, CenterZ);
+            cardPos.angle = -angle;
             cardPosList.Add(cardPos);
         }
         return cardPosList;
@@ -108,23 +158,40 @@ public class CardManager : MonoBehaviour
         return;
     }
 
+    // 指定カードのシンボルリストを取得するメソッド
+    public List<int> GetSymbolList(int cardID)
+    {
+        return dobbleCardArr[cardID];
+    }
+
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-
         // デモ用にカード配置を描画
-        List<CardPos> cardPosList = CalculateHandPos(5);
+        List<CardObject.CardPos> cardPosList = CalculateHandPos(5);
+        Gizmos.color = Color.red;
         foreach (var cardPos in cardPosList)
         {
-            Matrix4x4 original = Gizmos.matrix;
-            Gizmos.matrix = Matrix4x4.TRS(
-                cardPos.CenterPos,
-                transform.rotation * Quaternion.Euler(0, 0, -cardPos.Angle), // オブジェクトの回転に追加
-                Vector3.one
-            );
-            Gizmos.DrawWireCube(Vector3.zero, CardSize);
-            Gizmos.matrix = original;
+            DrawCardGizmo(cardPos);
         }
+
+        // ドロー時の出現位置
+        Gizmos.DrawSphere(CardStartPos, 0.1f);
+
+
+    }
+
+    // Gizmoで角度を含めて描画する
+    private void DrawCardGizmo(CardObject.CardPos cardPos)
+    {
+        Matrix4x4 original = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(
+            cardPos.center,
+            transform.rotation * Quaternion.Euler(0, 0, cardPos.angle), // オブジェクトの回転に追加
+            Vector3.one
+        );
+        Gizmos.DrawWireCube(Vector3.zero, CardSize);
+        Gizmos.matrix = original;
     }
 }
+
