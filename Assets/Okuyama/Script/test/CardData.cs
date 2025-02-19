fdc1bcf3532d81420e0c0c26b105e735b08268cd
Hiraisun <koushi.2nd@gmail.com>
2025-02-19 21:00:52 +0900

    適当なアニメーションを用意

---
+++ Assets/Okuyama/Script/test/CardData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// ステージに関する、スクリプトに影響を与える情報を扱う
+/// </summary>
+[CreateAssetMenu(fileName = "CardData", menuName = "ScriptableObjects/CardData")]
+public class CardData : ScriptableObject
+{
+
+    public IActivatable activatable;
+}
---
+++ Assets/Okuyama/Script/test/FactoryTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryTest : MonoBehaviour, IActivatable
+{
+	public void Activate()
+	{
+		// Implementation of the Activate method
+	}
+}
