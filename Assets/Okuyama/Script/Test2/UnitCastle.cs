906c026cf239bcca59c74c541a2343c555cc5ac2
Hiraisun <koushi.2nd@gmail.com>
2025-02-20 23:10:04 +0900

    本拠地をユニットとする

---
+++ Assets/Okuyama/Script/Test2/UnitCastle.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 本拠地(城)の処理
+/// 勝利/敗北判定
+/// </summary>
+public class UnitCastle : MonoBehaviour
+{
+    //TODO: 死亡時勝利/敗北
+}
