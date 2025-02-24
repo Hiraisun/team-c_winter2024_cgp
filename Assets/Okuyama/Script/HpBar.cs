121c7689a109c971bab5e3286b6224c38aa9c0bf
Hiraisun <koushi.2nd@gmail.com>
2025-02-24 20:17:21 +0900

    ダメージを受けずにいるとHPバーがフェードアウト

--- Assets/Okuyama/Script/HpBar.cs
+++ Assets/Okuyama/Script/HpBar.cs
@@ -1,8 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
-using UnityEngine.UI;
+
 
 /// <summary>
 /// HPバーの管理クラス
@@ -12,11 +10,15 @@ public class HpBar : MonoBehaviour
     [SerializeField] private RectTransform canvas;
     [SerializeField] private RectTransform front;
 
-    [SerializeField] private Image frontImage;
+    [SerializeField] private CanvasGroup canvasGroup; // 透明度の変更用
+
+    [Header("設定")]
+    [SerializeField] private float fadeDelay = 2f;    // フェード開始までの遅延時間
+    [SerializeField] private float fadeDuration = 1f; // フェードにかける時間
+
 
     private float maxHP;
-
-    // TODO: 最大HPの変化に対応
+    private Coroutine animationCoroutine; // コルーチン管理用
 
     /// <summary>
     /// 初期化
@@ -25,8 +27,12 @@ public class HpBar : MonoBehaviour
     {
         this.maxHP = maxHP;
 
+        // サイズ調整
         canvas.sizeDelta = new Vector2(width, height);
         front.offsetMax = new Vector2(0, 0);
+
+        // 透明にしておく
+        canvasGroup.alpha = 0;
     }
 
     /// <summary>
@@ -34,9 +40,36 @@ public class HpBar : MonoBehaviour
     /// </summary>
     public void UpdateHpBar(float currentHP)
     {
+        // バーの長さ更新
         float deltaX = - ( 1 - currentHP / maxHP) * canvas.sizeDelta.x;
         front.offsetMax = new Vector2(deltaX, 0);
+
+        // 既存のアニメーションを停止
+        if (animationCoroutine != null)
+        {
+            StopCoroutine(animationCoroutine);
+        }
+
+        // 新たにアニメーションを開始
+        animationCoroutine = StartCoroutine(HPbarAnimation());
     }
 
 
+    // 時間をかけて変化するアニメーション実装
+    private IEnumerator HPbarAnimation()
+    {
+        canvasGroup.alpha = 1f; // 不透明に
+
+        yield return new WaitForSeconds(fadeDelay);
+
+        // フェードアウト
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(1, 0f, timer / fadeDuration);
+            yield return null;
+        }
+        canvasGroup.alpha = 0f;
+    }
 }
