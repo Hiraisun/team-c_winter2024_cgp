23af722e7b227fdb8cd50f0d7cf09d1d88d8ba1c
Hiraisun <koushi.2nd@gmail.com>
2025-02-27 20:47:17 +0900

    Squashed commit of the following:
    commit 4e3b958865edaea04c830913bf1352424cbd5bc4
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Wed Feb 26 23:25:07 2025 +0900
    
        コルーチン削除
    
    commit f21b226469123c7765aab7df4b42b705814a3565
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Wed Feb 26 22:34:51 2025 +0900
    
        CardにMoveToメソッドを追加
    
    commit 87ac7e558dd96537545f7198b15c7045cdf7ec03
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Wed Feb 26 20:18:50 2025 +0900
    
        再配置ボタンの追加(デバッグ用)
    
    commit 452e836912185d32f161f5715ddbf1719a6b6b38
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Wed Feb 26 19:59:48 2025 +0900
    
        自動ドロー
    
    commit 58500342a01ee1cc0a0f38ef4d4991ba02d847d4
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Wed Feb 26 19:29:52 2025 +0900
    
        カードの配置を改良
    
    commit 02457389381b9ac1d15f978c8c502272e0b9ad48
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Wed Feb 26 15:32:27 2025 +0900
    
        配置要調整
    
    commit bc975c1975c92082483b8debbe0e3924141ae76f
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Wed Feb 26 06:45:17 2025 +0900
    
        トラッシュ仮
    
    commit 38df369ee73dbd411f02422b3541b825869ed2e6
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Wed Feb 26 03:53:11 2025 +0900
    
        ドローの仕組みを変更
    
    commit 7982b388f02ed9386d7314cf61592344f4e846fc
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Tue Feb 25 19:23:04 2025 +0900
    
        allSymbol自動代入
    
    commit 500507e57117db34bf7ecc8bcbdf5bce21f1924d
    Merge: af877fe f0c9de1
    Author: Hiraisun <koushi.2nd@gmail.com>
    Date:   Tue Feb 25 17:15:04 2025 +0900
    
        Merge branch 'main' into Tajiri
    
    commit af877fe8b271d72112c18e947c1ed7928ff645f0
    Author: Renren11212 <140488963+Renren11212@users.noreply.github.com>
    Date:   Tue Feb 25 16:56:13 2025 +0900
    
        さいご

--- Assets/Tajiri/Scripts/CardManager.cs
+++ Assets/Tajiri/Scripts/CardManager.cs
@@ -2,18 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEditor;
 
 public class CardManager : MonoBehaviour
 {
     // 使用するすべてのカード
     private List<List<int>> deck;
-
-    public List<List<int>> Deck
-    {
-        get { return deck; }
-        private set { deck = value; }
-    }
-    private int[] HandNums => cardCmps.Select(card => card.CardNum).ToArray();
 
     // 手札のゲームオブジェクト
     private GameObject[] cardObjs;
@@ -24,36 +18,53 @@ public class CardManager : MonoBehaviour
     private GameObject cardPrefab;
 
     [SerializeField, Header("１枚のカードに書かれているシンボルの数")]
-    private int SYMBOL_COUNT_PER_CARD = 4;
+    private int SYMBOL_PER_CARD = 4;
 
     [SerializeField, Header("手札の枚数")]
     private int INITIAL_HAND_CARDS = 5;
 
     [SerializeField, Header("手札の最大枚数")]
-    private int MAX_HAND_CARDS = 5;
+    private int MAX_HAND_CARDS = 8;
 
-    [SerializeField, Header("シンボルのデータを格納する")]
+    [SerializeField, Header("手札の位置")]
+    private Vector3 handPos;
+
+    [SerializeField, Header("カード間の隙間"), Range(1, 5)]
+    private float gapSizeMagnification = 3f;
+
+    private float gapSize;
+
+    private Vector3 trashPos = new(0, 10, 0);
+
     private SymbolData[] allSymbols;
 
-    public SymbolData[] AllSymbols
-    {
-        get { return allSymbols; }
-        private set { allSymbols = value; }
-    }
+    private int TOTAL_CARD_AND_SYMBOL => SYMBOL_PER_CARD * SYMBOL_PER_CARD - SYMBOL_PER_CARD + 1; // カードの枚数とシンボルの数は同じ
+
+    private int[] HandNums => cardCmps.Select(card => card.CardNum).ToArray();
 
     private void OnEnable()
     {
-        cardObjs = new GameObject[INITIAL_HAND_CARDS];
-        cardCmps = new Card[INITIAL_HAND_CARDS];
+        InitializeArrays();
 
         GenerateDobbleCardsList();
 
-        GenerateHandCards();
+        GenerateCardsObj();
+
+        InitialDraw();
+    }
+
+    private void InitializeArrays()
+    {
+        allSymbols = new SymbolData[TOTAL_CARD_AND_SYMBOL];
+        cardObjs = new GameObject[TOTAL_CARD_AND_SYMBOL];
+        cardCmps = new Card[TOTAL_CARD_AND_SYMBOL];
+
+        allSymbols = Resources.LoadAll<SymbolData>("Symbols");
     }
 
     private void GenerateDobbleCardsList()
     {
-        int n = SYMBOL_COUNT_PER_CARD - 1;
+        int n = SYMBOL_PER_CARD - 1;
 
         deck = new();
 
@@ -93,42 +104,119 @@ public class CardManager : MonoBehaviour
         }
     }
 
-    private void GenerateHandCards()
+    private void GenerateCardsObj()
     {
-        for (int i = 0; i < INITIAL_HAND_CARDS; i++)
+        for (int i = 0; i < TOTAL_CARD_AND_SYMBOL; i++)
         {
-            cardObjs[i] = Instantiate(cardPrefab, new(-7 + 3 * i + 1, -3, 0), Quaternion.identity); //配置に関しては一時的です
+            cardObjs[i] = Instantiate(cardPrefab, trashPos, Quaternion.identity); //配置に関しては一時的です
 
             cardCmps[i] = cardObjs[i].GetComponent<Card>();
-            cardCmps[i].Initialize(this);
+            cardCmps[i].SetCardInHand(false);
+            cardCmps[i].SetCardNum(i);
+            cardCmps[i].ApplyChanges(deck, allSymbols);
             cardCmps[i].OnCardClicked += HandleCardClicked;
-        }
-
-        for (int i = 0; i < INITIAL_HAND_CARDS; i++)
-        {
-            TrashAndDraw(cardCmps[i]);
         }
     }
 
-    private void TrashAndDraw(Card card)
+    private void InitialDraw()
     {
-        var availableNums = Enumerable.Range(0, deck.Count).Except(HandNums).ToList();
+        for (int i = 0; i < INITIAL_HAND_CARDS; i++)
+        {
+            Draw();
+        }
+    }
 
-        int newCardNum = availableNums[UnityEngine.Random.Range(0, availableNums.Count)];
+    private void Trash(Card selectedCard)
+    {
+        if (selectedCard == null)
+        {
+            Debug.LogWarning("捨てるカードが選択されていません。");
+            return;
+        }
 
-        card.SetCardNum(newCardNum);
-        card.ApplyChanges();
+        selectedCard.SetCardInHand(false);
+        selectedCard.MoveTo(trashPos, Quaternion.Euler(0, 0, 0));
+        RearrangeHand();
+    }
+
+    public void Draw()
+    {
+        if (cardCmps.Count(c => c.IsCardInHand) >= MAX_HAND_CARDS)
+        {
+            Debug.LogWarning("手札が最大枚数に達しています。");
+            return;
+        }
+
+        var inactiveCards = cardCmps.Where(c => !c.IsCardInHand).ToList();
+
+        if (inactiveCards.Count > 0)
+        {
+            Card newCard = inactiveCards[UnityEngine.Random.Range(0, inactiveCards.Count)];
+            newCard.SetCardInHand(true);
+            RearrangeHand();
+        }
+        else
+        {
+            Debug.LogWarning("非アクティブなカードがありません。");
+        }
+    }
+
+    public void RearrangeHand()
+    {
+        var cardsInHand = cardCmps.Where(c => c.IsCardInHand).ToList();
+        var (positions, rotations) = GetCardTransforms(cardsInHand.Count, 5f, 45f);
+
+        for (int i = 0; i < cardsInHand.Count; i++)
+        {
+            cardsInHand[i].MoveTo(positions[i], rotations[i]);
+        }
+    }
+
+    private (List<Vector3>, List<Quaternion>) GetCardTransforms(int cardCount, float radius, float maxAngle)
+    {
+        List<Vector3> positions = new();
+        List<Quaternion> rotations = new();
+
+        if (cardCount <= 0) return (positions, rotations);
+
+        float startAngle = -maxAngle * 0.5f;
+        float angleStep = (cardCount > 1) ? maxAngle / (cardCount - 1) : 0;
+        Vector3 center = handPos - new Vector3(0, radius, 0);
+
+        if (cardCount == 1)
+        {
+            Vector3 position = center + new Vector3(0 ,radius, 0);
+            positions.Add(position);
+
+            Quaternion rotation = Quaternion.Euler(0, 0, 0);
+            rotations.Add(rotation);
+
+            return (positions, rotations);
+        }
+
+        gapSize = cardCount * gapSizeMagnification * 0.1f;
+
+        for (int i = 0; i < cardCount; i++)
+        {
+            float angle = startAngle + angleStep * i;
+            float radian = angle * Mathf.Deg2Rad;
+
+            float x = Mathf.Sin(radian) * radius * gapSize;
+            float y = Mathf.Cos(radian) * radius;
+
+            Vector3 position = center + new Vector3(x, y, -i * 0.01f);
+            positions.Add(position);
+
+            Quaternion rotation = Quaternion.Euler(0, 0, -angle);
+            rotations.Add(rotation);
+        }
+
+        return (positions, rotations);
     }
 
     private Card selectedCard = null;
 
     private HashSet<int> matchingSymbols;
-
-    public List<int> MatchingSymbols
-    {
-        get { return matchingSymbols.ToList(); }
-        private set { matchingSymbols = value.ToHashSet(); }
-    }
 
     private Dictionary<int, SymbolData> commonSymbol;
 
@@ -152,7 +240,7 @@ public class CardManager : MonoBehaviour
                 {
                     if (_card != card)
                     {
-                        _card.SetHighlightedSymbol();
+                        _card.SetHighlightedSymbol(deck, matchingSymbols.ToList());
                     }
                 }
 
@@ -171,11 +259,11 @@ public class CardManager : MonoBehaviour
             }
             catch
             {
-                Debug.LogWarning(commonSymbol[card.CardNum].symbolSprite.name + " にアクションが設定されていません。");
+                Debug.LogWarning(commonSymbol[card.CardNum].symbolSprite.name + " に効果が設定されていません。");
             }
-    
-            TrashAndDraw(selectedCard);
-            TrashAndDraw(card);
+
+            Trash(selectedCard);
+            Trash(card);
 
             selectedCard = null;
 
@@ -191,3 +279,29 @@ public class CardManager : MonoBehaviour
         }
     }
 }
+
+/// <summary>
+/// エディターの設定
+/// </summary>
+#if UNITY_EDITOR
+[CustomEditor(typeof(CardManager))]
+public class CardManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        CardManager t = target as CardManager;
+
+        if (GUILayout.Button("ドロー"))
+        {
+            t.Draw();
+        }
+
+        if (GUILayout.Button("再配置"))
+        {
+            t.RearrangeHand();
+        }
+    }
+}
+#endif
