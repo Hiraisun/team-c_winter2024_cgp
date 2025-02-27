293d1d1cdb17440a8f349796167111991325292f
Hiraisun <koushi.2nd@gmail.com>
2025-02-28 03:39:32 +0900

    リファクタリング2：Obserber風の実装

--- Assets/Tajiri/Scripts/Card.cs
+++ Assets/Tajiri/Scripts/Card.cs
@@ -3,6 +3,7 @@ using System;
 using DG.Tweening;
 using System.Linq;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 
 
 /// <summary>
@@ -16,81 +17,236 @@ public class Card : MonoBehaviour
     // カードの効果説明
     public string EffectDiscription { get; private set; }
 
-    // 手札にあるかどうか
-    private bool isCardInHand;
-    public bool IsCardInHand { get; private set; }
+    public enum CardState{
+        InDeck,
+        Hand_Unselected,
+        Hand_Selected,
+        Used,
+        Trashed
+    }
+    private CardState state; // カードの状態
+    public CardState State { get => state; }
+
+    // 手札にあるかどうか ( Hand_Unselected または Hand_Selected かどうか)
+    public bool IsCardInHand { get => state == CardState.Hand_Unselected || state == CardState.Hand_Selected; }
 
     [SerializeField, Header("シンボルを表示するSpriteRenderer")]
     private SpriteRenderer[] sr;
 
-    // カードがクリックされたときに発火
+    // カードがクリックされたときのイベント
     private event Action<Card> OnCardClicked;
-    // イベントリスナーを追加
     public void AddCardClickedListener(Action<Card> listener) => OnCardClicked += listener;
 
+    // カードマネージャーの参照
+    private CardManager cardManager;
+
+    // シンボルリスト
+    private List<int> symbols;
+    // シンボルのHashSet
+    public HashSet<int> SymbolsHashSet { get; private set; }
+
+    // 手札として持つ際の位置情報キャッシュ
+    private Vector3 handPosition;
+    private Quaternion handRotation;
+    // TODO:選択状態での位置情報 (暫定的に少し上に)
+    private Vector3 selectedPosition { get => handPosition + Vector3.up*1f; }
+    private Quaternion selectedRotation { get => handRotation; }
 
     /// <summary>
     /// 初期化処理を一元化したメソッド
     /// </summary>
-    public void Initialize(int cardNum, List<List<int>> deck, SymbolData[] allSymbols)
+    public void Initialize(int cardNum, CardManager cardManager)
     {
-        CardNum = cardNum;
-        IsCardInHand = false; // 初期状態は手札外
+        this.cardManager = cardManager;
+        this.CardNum = cardNum;
+
+        state = CardState.InDeck; //初期状態
+        transform.position = cardManager.DrawPos;
+
+        // カードデータ初期化
+        this.symbols = cardManager.Deck[CardNum];
+        this.SymbolsHashSet = new HashSet<int>(symbols);
+
+        // イベントハンドラの登録
+        cardManager.AddCardSelectedListener(HandleCardSelected);
+        cardManager.AddCardDeselectedListener(HandleCardDeselected);
+        cardManager.AddCardUsedListener(HandleCardUsed);
 
         // シンボルスプライトの初期化
         for (int i = 0; i < sr.Length; i++)
         {
-            int symbolIndex = deck[CardNum][i];
-            sr[i].sprite = allSymbols[symbolIndex].symbolSprite;
+            int symbolIndex = symbols[i];
+            sr[i].sprite = cardManager.AllSymbolData[symbolIndex].symbolSprite;
         }
 
         // TODO: 効果説明の初期化
     }
 
     /// <summary>
-    /// カードを手札に加える
-    /// それに伴った演出も行う
+    /// 手札座標の再セット
     /// </summary>
-    public void ToHand()
+    public void SetHandPosition(Vector3 position, Quaternion rotation)
     {
-        IsCardInHand = true;
-    }
+        handPosition = position;
+        handRotation = rotation;
 
-
-
-    public void SetHighlightedSymbol(List<List<int>> deck, List<int> matchingSymbols)
-    {
-        int highlightedSymbol = deck[CardNum].Intersect(matchingSymbols).DefaultIfEmpty(-1).First();
-        int highlightedSymbolIndex = deck[CardNum].IndexOf(highlightedSymbol);
-
-        if (highlightedSymbolIndex == -1) return;
-        sr[highlightedSymbolIndex].DOKill();
-        sr[highlightedSymbolIndex].DOColor(Color.green, 1f);
-    }
-
-    public void ResetSymbolsColor()
-    {
-        foreach(SpriteRenderer _sr in sr)
-        {
-            _sr.DOKill();
-            _sr.DOColor(Color.white, 1f);
+        // 手札状態なら移動
+        if (IsCardInHand) {
+            MoveTo(handPosition, handRotation);
         }
     }
 
     /// <summary>
-    /// カードを指定した座標/回転角に1秒かけて移動
+    /// カードを手札に加える  -------------(1)
     /// </summary>
-    public void MoveTo(Vector3 position, Quaternion rotation)
+    public void Draw()
     {
-        this.transform.DOMove(position, 1f);
-        this.transform.DORotateQuaternion(rotation, 1f);
+        if(state!=CardState.InDeck){
+            Debug.LogError("カードがデッキにない");
+            return;
+        }
+
+        state = CardState.Hand_Unselected;
+    }
+
+    /// <summary>
+    /// 手札のカードを選択する -------------(2)
+    /// </summary>
+    public void Select()
+    {
+        if(state!=CardState.Hand_Unselected){
+            Debug.LogError("カードが手札にない");
+            return;
+        }
+
+        state = CardState.Hand_Selected;
+
+        // 選択時演出
+        MoveTo(selectedPosition, selectedRotation);
+        //TODO:枠線
+    }
+
+    /// <summary>
+    /// 手札のカードを選択解除する-------------(3)
+    /// </summary>
+    public void Deselect()
+    {
+        if(state!=CardState.Hand_Selected){
+            Debug.LogError("カードが選択されていない");
+            return;
+        }
+
+        state = CardState.Hand_Unselected;
+
+        // 選択解除演出
+        MoveTo(handPosition, handRotation);
+    }
+
+    /// <summary>
+    /// カードを使用する -------------(4)
+    /// </summary>
+    public async void Use()
+    {
+        if(state!= CardState.Hand_Selected  && state!= CardState.Hand_Unselected){
+            Debug.LogError("カードが手札にない状態で使用された");
+            return;
+        }
+
+        state = CardState.Used;
+        
+        // 使用時演出待機
+        await UseVisualEffect();
+
+        state = CardState.InDeck;
+        transform.position = cardManager.DrawPos;
+    }
+    // カード使用時演出
+    private async UniTask UseVisualEffect()
+    {
+        // TODO:暫定的にぐるぐる回してます
+        transform.DOKill();
+        Sequence sequence = DOTween.Sequence()
+            .Append(transform.DOMove(cardManager.UsedPos, 1f))
+            .Join(transform.DORotate(new Vector3(0, 0, 1080), 2f, RotateMode.FastBeyond360).SetEase(Ease.InQuad))
+            .Join(transform.DOScale(Vector3.zero, 2f).SetEase(Ease.InQuad));
+        await sequence.Play().ToUniTask();
+
+        transform.rotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+    }
+
+
+
+    /// <summary>
+    /// カード選択のイベントハンドラ 何らかのカードが選択されたとき
+    /// </summary>
+    public void HandleCardSelected(Card selectedCard)
+    {
+        if(selectedCard != this)// 他のカードが選択された
+        {
+            // シンボルハイライト演出
+            StartSymbolHighlight(selectedCard);
+        }
+    }
+    /// <summary>
+    /// カード選択解除のイベントハンドラ
+    /// </summary>
+    public void HandleCardDeselected(Card deselectedCard)
+    {
+        if(deselectedCard != this) // 他のカードが選択解除
+        {
+            // シンボルハイライト演出終了
+            FinishSymbolHighlight();
+        }
+    }
+    /// <summary>
+    /// カード使用のイベントハンドラ
+    /// </summary>
+    public void HandleCardUsed()
+    {
+        // シンボルハイライト演出終了
+        FinishSymbolHighlight();
+    }
+    
+    /// <summary>
+    /// シンボルハイライト演出を開始
+    /// </summary>
+    private void StartSymbolHighlight(Card selectedCard)
+    {
+        // 一致判定
+        int highlightSymbol = selectedCard.SymbolsHashSet.Intersect(SymbolsHashSet).FirstOrDefault();
+        int highlightSymbolIndex = symbols.IndexOf(highlightSymbol);
+
+        if (highlightSymbolIndex == -1) return;
+        sr[highlightSymbolIndex].DOKill();
+        sr[highlightSymbolIndex].DOColor(Color.green, 0.5f);
+    }
+    /// <summary>
+    /// シンボルハイライト演出を終了
+    /// </summary>
+    private void FinishSymbolHighlight()
+    {
+        foreach(SpriteRenderer _sr in sr)
+        {
+            _sr.DOKill();
+            _sr.DOColor(Color.white, 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// カードを指定した座標/回転角に移動する演出
+    /// </summary>
+    private void MoveTo(Vector3 position, Quaternion rotation, float duration = 0.3f)
+    {
+        DOTween.Kill(transform);
+        transform.DOMove(position, duration);
+        transform.DORotateQuaternion(rotation, duration);
     }
 
     //カプセル化する意味がなくなる...
     //public void SetCardNum(int value) => CardNum = value;
     //public void SetCardInHand(bool value) => IsCardInHand = value;
     //public void SetCardDescription(string value) => EffectDiscription = value;
-
 
     // このオブジェクトがクリックされたとき
     private void OnMouseDown()
@@ -101,11 +257,11 @@ public class Card : MonoBehaviour
     // マウスホバー時の拡大縮小
     private void OnMouseEnter()
     {
-        this.transform.DOScale(Vector2.one * 1.1f, 0.2f);
+        if (IsCardInHand) transform.DOScale(Vector2.one * 1.1f, 0.2f);
     }
     private void OnMouseExit()
     {
-        this.transform.DOScale(Vector2.one, 0.2f);
+        if (IsCardInHand) transform.DOScale(Vector2.one, 0.2f);
     }
 }
 
--- Assets/Tajiri/Scripts/CardManager.cs
+++ Assets/Tajiri/Scripts/CardManager.cs
@@ -8,7 +8,7 @@ using UnityEngine.XR;
 public class CardManager : MonoBehaviour
 {
     // 使用するすべてのカード
-    private List<List<int>> deck;
+    public List<List<int>> Deck{ get; private set; }
 
     // 全カードのゲームオブジェクト配列
     private GameObject[] cardObjs;
@@ -25,7 +25,6 @@ public class CardManager : MonoBehaviour
     [SerializeField, Header("手札の最大枚数")]
     private int MAX_HAND_CARDS = 8;
 
-    [Space(10)]
     [SerializeField, Header("手札位置の調整パラメータ")]
     private CardAlgorithms.HandPosConfig handPosConfig = new()
     {
@@ -35,38 +34,60 @@ public class CardManager : MonoBehaviour
         gapSizeMagnification = 3f
     };
 
+    [Space(10)]
     [SerializeField, Header("ドロー時出現位置")]
-    private Vector3 drawPos = new(0, 10, 0); //初期位置として利用
-
+    private Vector3 drawPos = new(-10, 0, 0);
+    public Vector3 DrawPos { get => drawPos; }
+    [SerializeField, Header("使用時移動先")]
+    private Vector3 usedPos = new(10, 0, 0);
+    public Vector3 UsedPos { get => usedPos; }
     [SerializeField, Header("トラッシュ時移動先")]
-    private Vector3 trashPos = new(0, 0, 0);
+    private Vector3 trashPos = new(0, 10, 0);
+    public Vector3 TrashPos { get => trashPos; }
 
-    private SymbolData[] allSymbols;
+    // ScriptableObjectからロードしたシンボルデータ
+    private SymbolData[] allSymbolData;
+    public SymbolData[] AllSymbolData{ get => allSymbolData; }
 
     private int TOTAL_CARD_AND_SYMBOL => SYMBOL_PER_CARD * SYMBOL_PER_CARD - SYMBOL_PER_CARD + 1; 
     // カードの枚数とシンボルの数は同じ
 
     private int[] HandNums => cardCmps.Select(card => card.CardNum).ToArray();
 
+
+    // 手札選択イベント
+    private event Action<Card> OnCardSelected;
+    public void AddCardSelectedListener(Action<Card> listener) => OnCardSelected += listener;
+    // 手札選択解除イベント
+    private event Action<Card> OnCardDeselected;
+    public void AddCardDeselectedListener(Action<Card> listener) => OnCardDeselected += listener;
+    // 手札使用イベント
+    private event Action OnCardUsed;
+    public void AddCardUsedListener(Action listener) => OnCardUsed += listener;
+
+
     // 選択中のカード
     private Card selectedCard = null;
+
+
+
 
     // 初期化処理
     private void Awake()
     {
         InitializeArrays();
-        deck = CardAlgorithms.GenerateDobbleCardsList(SYMBOL_PER_CARD);
+        Deck = CardAlgorithms.GenerateDobbleCardsList(SYMBOL_PER_CARD);
         GenerateCardsObj();
     }
 
     // メモリ確保
     private void InitializeArrays()
     {
-        allSymbols = new SymbolData[TOTAL_CARD_AND_SYMBOL];
+        allSymbolData = new SymbolData[TOTAL_CARD_AND_SYMBOL];
         cardObjs = new GameObject[TOTAL_CARD_AND_SYMBOL];
         cardCmps = new Card[TOTAL_CARD_AND_SYMBOL];
 
-        allSymbols = Resources.LoadAll<SymbolData>("Symbols");
+        allSymbolData = Resources.LoadAll<SymbolData>("Symbols");
     }
 
     // カードオブジェクトの生成(初期化)
@@ -79,54 +100,93 @@ public class CardManager : MonoBehaviour
 
             // 参照取得処理
             cardCmps[i] = cardObjs[i].GetComponent<Card>();
-            cardCmps[i].Initialize(i, deck, allSymbols);            // 初期化
-            cardCmps[i].AddCardClickedListener(HandleCardClicked);  // クリックイベントを聴く
+            cardCmps[i].Initialize(i, this);       // 初期化
+            cardCmps[i].AddCardClickedListener(HandleCardClicked); // クリックイベントの登録
         }
     }
 
     /// <summary>
-    /// カードを1枚引く
+    /// カードを1枚引く   ---------------------(1)
     /// </summary>
-    public void Draw()
+    public void DrawCard()
     {
-        // 例外処理
+        // 上限処理
         if (cardCmps.Count(c => c.IsCardInHand) >= MAX_HAND_CARDS)
         {
             Debug.LogWarning("手札が最大枚数に達しています。");
             return;
         }
 
-        // 手札にないカード
-        var inactiveCards = cardCmps.Where(c => !c.IsCardInHand).ToList();
+        // Deckのカードを取得
+        var inDeckCards = cardCmps.Where(c => c.State == Card.CardState.InDeck).ToList();
 
-        if (inactiveCards.Count > 0)
+        if (inDeckCards.Count > 0)
         {
-            Card newCard = inactiveCards[UnityEngine.Random.Range(0, inactiveCards.Count)];
-            newCard.ToHand(); // 手札に加える
+            Card newCard = inDeckCards[UnityEngine.Random.Range(0, inDeckCards.Count)];
+            newCard.Draw();
             RearrangeHand();
         }
         else
         {
-            Debug.LogWarning("非アクティブなカードがありません。");
+            Debug.LogWarning("デッキにカードがありません。");
         }
     }
 
-    ///// <summary>
-    ///// 指定したカードを捨てる   ->   「捨てる」「使う」は別で定義すべきかも
-    ///// </summary>
-    ///// <param name="selectedCard"></param>
-    //private void Trash(Card selectedCard)
-    //{
-    //    if (selectedCard == null)
-    //    {
-    //        Debug.LogWarning("捨てるカードが選択されていません。");
-    //        return;
-    //    }
-    //
-    //    selectedCard.SetCardInHand(false);
-    //    selectedCard.MoveTo(trashPos, Quaternion.Euler(0, 0, 0));
-    //    RearrangeHand();
-    //}
+    /// <summary>
+    /// カードを選択する ----------------------(2)
+    /// </summary>
+    public void SelectCard(Card card)
+    {
+        card.Select();
+        selectedCard = card;
+        OnCardSelected?.Invoke(card);
+    }
+
+    /// <summary>
+    /// カードの選択を解除する-----------------(3)
+    /// </summary>
+    public void DeselectCard(Card card)
+    {
+        if(card != selectedCard)
+        {
+            Debug.LogWarning("選択中のカードではありません。");
+            return;
+        }
+        card.Deselect();
+        selectedCard = null;
+        OnCardDeselected?.Invoke(card);
+    }
+
+    /// <summary>
+    /// 指定した2枚のカードを使用する -----------------(4)
+    /// </summary>
+    public void ActivateCards(Card card1, Card card2)
+    {
+        // 重複するシンボルを取得
+        var commonSymbolIDs = card1.SymbolsHashSet.Intersect(card2.SymbolsHashSet).ToList();
+
+        // シンボルの数が1つの場合
+        if (commonSymbolIDs.Count == 1)
+        {
+            // シンボルに対応する効果を発動
+            try{
+                AllSymbolData[commonSymbolIDs[0]].cardAction.Activate(OwnerType.PLAYER);
+            }
+            catch (NullReferenceException)
+            {
+                Debug.LogWarning("効果未設定 : " + AllSymbolData[commonSymbolIDs[0]].description);
+            }
+            card1.Use();
+            card2.Use();
+            selectedCard = null;
+            OnCardUsed?.Invoke();
+            RearrangeHand();
+        }
+        else
+        {
+            Debug.LogError("重複シンボルが1つじゃない。"+ commonSymbolIDs.ToString());
+        }
+    }
 
 
     /// <summary>
@@ -139,7 +199,7 @@ public class CardManager : MonoBehaviour
 
         for (int i = 0; i < cardsInHand.Count; i++)
         {
-            cardsInHand[i].MoveTo(positions[i], rotations[i]);
+            cardsInHand[i].SetHandPosition(positions[i], rotations[i]);
         }
     }
 
@@ -149,66 +209,26 @@ public class CardManager : MonoBehaviour
     /// !!! イベントハンドリング (クリック時の処理の振り分け) のみを行う !!!
     /// 気を抜くとここが肥大化する。分散しよう。
     /// 
+    /// ((そもそもイベントである必要がない説がある。))
     /// </summary>
     private void HandleCardClicked(Card card)
     {
-        if (selectedCard == null) // 選択中でない 
+        if (selectedCard == null) // 何も選択中じゃない
         {
-            selectedCard = card;
-
-            HashSet<int> candidateOfSymbols = new(deck[selectedCard.CardNum]);
-
-            Dictionary<int, SymbolData> commonSymbol = new();
-
-            HashSet<int> matchingSymbols;
-
-            foreach (int handNum in HandNums)
-            {
-                HashSet<int> handSymbols = new(deck[handNum]);
-
-                matchingSymbols = new(candidateOfSymbols.Intersect(handSymbols));
-
-                foreach (Card _card in cardCmps)
-                {
-                    if (_card != card)
-                    {
-                        _card.SetHighlightedSymbol(deck, matchingSymbols.ToList());
-                    }
-                }
-
-                foreach (int matchingSymbol in matchingSymbols)
-                {
-                    commonSymbol[handNum] = allSymbols[matchingSymbol];
-                }
-            }
+            SelectCard(card);
         }
         else if (selectedCard != card) // 選択中, かつ他のカードをクリック
         {
-            try
-            {
-                //commonSymbol[card.CardNum].cardAction.Activate(OwnerType.PLAYER);
-            }
-            catch
-            {
-                //Debug.LogWarning(commonSymbol[card.CardNum].symbolSprite.name + " に効果が設定されていません。");
-            }
-
-            //Trash(selectedCard);
-            //Trash(card);
-
-            selectedCard = null;
-
-            foreach (Card _card in cardCmps)
-            {
-                _card.ResetSymbolsColor();
-            }
+            ActivateCards(selectedCard, card);
         }
-
-        else
+        else // 同じカードをクリック
         {
-            Debug.LogWarning("同じカードは選択できません");
+            DeselectCard(card);
         }
     }
+
+
+
 }
 
 /// <summary>
@@ -226,7 +246,7 @@ public class CardManagerEditor : Editor
 
         if (GUILayout.Button("ドロー"))
         {
-            t.Draw();
+            t.DrawCard();
         }
 
         if (GUILayout.Button("再配置"))
